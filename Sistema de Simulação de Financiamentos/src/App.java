1332c5fe31605b7704bb59289cfedd7702df3bb2
jooj finalboss <joaovitor02.2000@gmail.com>
2023-09-26 09:46:41 -0300

    v0.2 - Primeiras Classes

--- Sistema de Simulação de Financiamentos/src/App.java
+++ Sistema de Simulação de Financiamentos/src/App.java
@@ -1,5 +1,56 @@
+import java.util.Scanner;
+
 public class App {
+
+    public static class Financiamento {
+        double valorImovel;
+        int prazoFinanciamento;
+        double taxaJurosAnual;
+        
+        void setFinanciamento(double valorImovel, int prazoFinanciamento, double taxaJurosAnual) {
+            this.valorImovel = valorImovel;
+            this.prazoFinanciamento = prazoFinanciamento;
+            this.taxaJurosAnual = taxaJurosAnual;
+        }
+
+        double calcularPagamentoMensal(){
+            double pagamentoMensal = (valorImovel / prazoFinanciamento) * (1 + taxaJurosAnual / 12);
+            return pagamentoMensal;
+        }
+        double calcularPagamentoTotal(double pagamentoMensal){
+            double pagamentoTotal = pagamentoMensal * prazoFinanciamento;
+            return pagamentoTotal;
+        }
+        
+    }
+    public class InterfaceUsuario {
+        static double pedirValorImovel(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite o valor do imóvel:\t");
+            double valorImovel = scanner.nextDouble();
+            return valorImovel;
+        }
+        static int pedirPrazoFinanciamento(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite o prazo de financiamento em anos:\t");
+            int prazoFinanciamento = scanner.nextInt();
+            return prazoFinanciamento;
+        }
+        static double pedirTaxaJurosAnual(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite o valor da taxa de juros anual:\t");
+            double taxaJurosAnual = scanner.nextDouble();
+            return taxaJurosAnual;
+        }
+    }
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double valorImovel = InterfaceUsuario.pedirValorImovel();
+        int prazoFinanciamento = InterfaceUsuario.pedirPrazoFinanciamento();
+        double taxaJurosAnual = InterfaceUsuario.pedirTaxaJurosAnual();
+
+        Financiamento FinanciamentoAtual = new Financiamento();
+        FinanciamentoAtual.setFinanciamento(valorImovel, prazoFinanciamento, taxaJurosAnual);
     }
 }
